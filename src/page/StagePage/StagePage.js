13fe78569bc68016b79859c9c232c1326a78bac7
kiri-91 <kirifog91@gmail.com>
2022-10-18 09:44:11 +0900

    ステージ選択画面実装 デザインは改善の余地あり

--- src/page/StagePage/StagePage.js
+++ src/page/StagePage/StagePage.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from "./StagePage.module.css";
+
+function StageButton(props) {
+    return (
+        <Link to={'/game/'}>
+            <button className={props.className}>PUSH</button>
+        </Link>
+    )
+}
 
 export default function StagePage() {
     return (
         <>
-            <h1>StagePage</h1>
-            <div>
+            <div className={styles.stages}>
+                <h1 className='title'>ステージ選択</h1>
+                <div className={styles.buttons1}>
+                    <StageButton className={styles.topleft}/>
+                    <StageButton className={styles.topcenter}/>
+                    <StageButton className={styles.topright}/>
+                </div>
+                <div className={styles.buttons2}>
+                    <StageButton className={styles.bottomleft}/>
+                    <StageButton className={styles.bottomcenter}/>
+                    <StageButton className={styles.bottomright}/>
+                </div>
                 <Link to={'/game/'}>ゲーム画面へ</Link>
             </div>
         </>
