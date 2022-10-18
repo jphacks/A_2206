d662a3e555ed4fa0d5d9680b185ee3e43e3de00e
kiri <81542048+kiri-91@users.noreply.github.com>
2022-10-18 22:18:58 +0900

    Revert "Revert "lintとprettirの設定ファイルを加えた""

--- src/page/HomePage/HomePage.js
+++ src/page/HomePage/HomePage.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
     return (
         <>
             <h1>HomePage</h1>
             <div>
-                <Link to={'/goal/'}>目標設定画面へ</Link>
-                <Link to={'/course/'}>コース選択画面へ</Link>
+                <Link to={"/goal/"}>目標設定画面へ</Link>
+                <Link to={"/course/"}>コース選択画面へ</Link>
             </div>
         </>
-    )
+    );
 }
--- src/page/LoginPage/LoginPage.js
+++ src/page/LoginPage/LoginPage.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function LoginPage() {
     return (
         <>
             <h1>LoginPage</h1>
             <div>
-                <Link to={'/home/'}>Homeへ</Link>
+                <Link to={"/home/"}>Homeへ</Link>
             </div>
         </>
-    )
+    );
 }
