e0f71c61aab3ebdf0793920c9a27b3b4313b27f3
kiri-91 <kirifog91@gmail.com>
2022-10-19 22:46:23 +0900

    tmp

--- src/page/GamePage/GamePage.js
+++ src/page/GamePage/GamePage.js
@@ -1,3 +1,16 @@
+import React,{ useRef, useState, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+// TODO1 - import required model here
+// e.g.
+import Webcam from 'react-webcam';
+import "./GamePage.module.css";
+// TODO2 - import drawing utility here
+
+const runCoco = async () => {
+    // 3. TODO - Load network
+     const net = await tf.loadGraphModel('url')
+}
+
 export default function GamePage() {
     return (
         <>
