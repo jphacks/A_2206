e189eb259badf95c7a0d9ab41f88e82f4320e526
kiri-91 <kirifog91@gmail.com>
2022-10-20 01:17:30 +0900

    tmp

--- src/page/GamePage/GamePage.js
+++ src/page/GamePage/GamePage.js
@@ -8,7 +8,33 @@ import "./GamePage.module.css";
 
 const runCoco = async () => {
     // 3. TODO - Load network
-     const net = await tf.loadGraphModel('url')
+    const net = await tf.loadGraphModel('url')
+
+    setInterval(() => {
+        detect(net);
+    }, 16.7)
+}
+
+const detect = async (net) => {
+    // check data is available
+    if (
+        typeof webcamRef.current !== "undefined" &&
+        webcamRef.current !== null &&
+        webcamRef.current.video.readyState === 4
+    ) {
+        // Get Video Properties
+        const video = webcamRef.current.video;
+        const videoWidth = webcamRef.current.video.videoWidth;
+        const videoHeight = webcamRef.current.video.videoHeight;
+
+        // Set video width
+        webcamRef.current.video.width = videoWidth;
+        webcamRef.current.video.height = videoHeight;
+
+        // Set canvas height and width;
+        canvasRef.current.width = videoWidth;
+        canvasRef.current.height = videoHeight;
+    }
 }
 
 export default function GamePage() {
